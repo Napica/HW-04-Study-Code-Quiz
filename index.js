78caa9dac79fde6b7519afda9fd6ff8d59653ec9
Archangel <nathapcal@gmail.com>
2020-09-09 21:07:18 -0400

    worked with tutor to create timer, set questions object array, and created a way to interact with the answer sections

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 // Elements to pull from
 var startPromptEl = document.getElementById("startPromt");
 var startEl = document.querySelector(".start");
-var startBtnEl = document.getElementById("start-btn")
+var startBtnEl = document.querySelector("#start-Btn");
 var questionBoxEl = document.getElementById("quizQuestionBox");
 var submitEl = document.getElementById("submit-score");
 var questionEl = document.getElementById("question");
 var answerEl = document.getElementById("answerButtons");
 var timerEl = document.getElementById("time");
+var secondsLeft = 180;
+
 var score = 0;
 var currentQuestionIndex = 0;
-
 
 // Arrary of objects to contain questions and answers
 var questions = [
   {
     question: "Commonly used data types DO NOT include: ",
-    choices: ["1) Strings, 2) Booleans, 3) Alerts, 4) Numbers"],
-    correctAnswer: "3) alerts"
+    choice1: ["1) Strings", "2) Booleans", "3) Alerts", "4) Numbers"],
+    correctAnswer: "3) alerts",
   },
   {
     question: "The condition of an If/Else statement is enclosed within____.",
-    choice1: ["1) Quotes, 2) Curley Brackets, 3) Parentheses, 4) Square Brackets"],
+    choice1: [
+      "1) Quotes, 2) Curley Brackets, 3) Parentheses, 4) Square Brackets",
+    ],
     correctAnswer: "2) Curley Brackets",
   },
   {
     question: "Arrays in JavaScript can be used to store _______.",
-    choice1: ["1) Numbers and Strings,2) Other Arrays, 3) Booleans, 4) All of the above"],
+    choice1: [
+      "1) Numbers and Strings,2) Other Arrays, 3) Booleans, 4) All of the above",
+    ],
     correctAnswer: "4) All of the above",
   },
   {
@@ -37,21 +42,42 @@ var questions = [
   {
     question:
       "A very useful tool used during development and debugging for printing content to the debugger is: ",
-    choice1: ["1) console.log(), 2) JavaScript, 3) for loops, 4) If/Else conditionals"],
+    choice1: [
+      "1) console.log(), 2) JavaScript, 3) for loops, 4) If/Else conditionals",
+    ],
     correctAnswer: "1) console.log()",
   },
 ];
 
+startBtnEl.addEventListener("click", function(){
+    startEl.classList.add("hide");
+    questionBoxEl.classList.remove("hide");
+    // console.log("this works")
+    showQuestion();
+    setTimer();
+});
 
-startBtnEl.addEventListener("click", startQuiz);
 
-function startQuiz() {
-  startEl.classList.add("hide");
-  questionBoxEl.classList.remove("hide");
+function showQuestion(){
+  if (currentQuestionIndex < questions.length) {
+    questionEl.innerHTML = questions[currentQuestionIndex].question;
+    for (var i = 0; i < questions[currentQuestionIndex].choice1.length; i++){
+      var newAnswer = document.createElement("button");
+      newAnswer.textContent = questions[currentQuestionIndex].choice1[i];
+      newAnswer.setAttribute("data-name", "data-choice-" + i );
+      answerEl.append(newAnswer);
+    }
+  }
 }
 
-
-
-function showQuestion(question){
-  questionEl.innerText = question.question;
+function setTimer() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timerEl.textContent = secondsLeft;
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      questionBoxEl.classList.add("hide");
+      // add submit score section. remove hide
+    }
+  }, 1000);
 }
